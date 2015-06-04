75f0e860c120dc0b26e72cc5b11b42d1a44ab4d2
Matthew Giardina <mlgiardina@me.com>
2015-06-04 15:55:03 -0500

    added some actions to messages controller

--- number_one_chat/app/controllers/messages_controller.rb
+++ number_one_chat/app/controllers/messages_controller.rb
@@ -2,4 +2,10 @@ class MessagesController < ApplicationController
 
   def index
     render json: Message.all
+  end
+
+  def create
+    new_message = Message.create(user: params[:user], chatroom: params[:chatroom], message: params[:message])
+    render json: new_message
+  end
 end
--- number_one_chat/config/routes.rb
+++ number_one_chat/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'messages', to: 'messages#index'
+
+  post 'messages', to: 'messages#create'
+
   root to: 'messages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
