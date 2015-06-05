4839df9c252fc38f4a86d050dac7add1de813ab5
Matthew Giardina <mlgiardina@me.com>
2015-06-05 09:50:39 -0500

    removed chatroom_id column from users table

--- number_one_chat/db/schema.rb
+++ number_one_chat/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605143219) do
+ActiveRecord::Schema.define(version: 20150605144805) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.string   "name"
@@ -29,9 +29,8 @@ ActiveRecord::Schema.define(version: 20150605143219) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.integer  "chatroom_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
