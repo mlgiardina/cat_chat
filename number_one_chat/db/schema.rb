8ed999cdca817d3b79d9a7ed90241b062e0daff3
Matthew Giardina <mlgiardina@me.com>
2015-06-05 15:27:28 -0500

    i think i fixed it

--- number_one_chat/db/schema.rb
+++ number_one_chat/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605144805) do
+ActiveRecord::Schema.define(version: 20150605201749) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.string   "name"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20150605144805) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.integer  "user_id",     null: false
-    t.integer  "chatroom_id", null: false
+    t.integer  "user_id"
+    t.integer  "chatroom_id"
     t.text     "message"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
--- number_one_chat/db/seeds.rb
+++ number_one_chat/db/seeds.rb
@@ -4,6 +4,10 @@ Message.create(user_id: 1, chatroom_id: 2, message: "Test 4")
 Message.create(user_id: 1, chatroom_id: 2, message: "Test 3")
 Message.create(user_id: 1, chatroom_id: 2, message: "Test 2")
 Message.create(user_id: 1, chatroom_id: 2, message: "Test 1")
+User.create(id: 1, name: "Keith")
+User.create(id: 2, name: "Matthew")
+User.create(id: 3, name: "Kristen")
+Chatroom.create(name: "The Living Room")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
