0cde518e308adae2dc45402f15f6836e87bea999
Matthew Giardina <mlgiardina@me.com>
2015-06-05 14:40:33 -0500

    getting ready to change heroku stuff

--- number_one_chat/app/controllers/messages_controller.rb
+++ number_one_chat/app/controllers/messages_controller.rb
@@ -1,16 +1,16 @@
 class MessagesController < ApplicationController
 
   def index
-    message_object = Message.all
-    blah = []
-      message_object.each do |message|
+    messages_object = Message.all
+     all_messages = []
+      messages_object.each do |message|
         deliverable = {}
         deliverable["user"] = message.user.name
         deliverable["chatroom"] = message.chatroom.name
         deliverable["message"] = message.message
-        blah.push(deliverable)
+        all_messages.push(deliverable)
       end
-    render json: blah
+    render json: all_messages
   end
 
   def create
@@ -23,6 +23,10 @@ class MessagesController < ApplicationController
     end
   end
 
-  def display_stats
-  end
+  # def display_stats
+  #   users = []
+  #   users.push(User.)
+  #   topTenUsers =
+  #   render json: { mostPopularChatroom, topTenUsers, recentlyActiveUsers }
+  # end
 end
