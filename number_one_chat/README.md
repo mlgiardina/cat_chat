2e60659270e7c981c14cd675139f6ded7d08c12d
Matthew Giardina <mlgiardina@me.com>
2015-06-07 14:17:59 -0500

    adding new words to swear filter

---
+++ number_one_chat/README.md
@@ -0,0 +1,3 @@
+#Cat Chat
+##Description
+I was part of a three-person team that created a chat application. I wrote the backend for our application, which is the code in this repository.
--- number_one_chat/app/controllers/messages_controller.rb
+++ number_one_chat/app/controllers/messages_controller.rb
@@ -21,6 +21,8 @@ class MessagesController < ApplicationController
     @censored_message.gsub!(/ass\b/, 'beep')
     @censored_message.gsub!(/ass/, 'beep')
     @censored_message.gsub!(/bitch/, 'beep')
+    @censored_message.gsub!(/vagina/, 'beep')
+    @censored_message.gsub!(/penis/), 'beep')
   end
 
   def check_bot_response
